f6a82bb55ae1f909edeffbbc8b7ce6f6d0bc87d8
Joffrey Sterckx <147731791+SterckxJoffrey@users.noreply.github.com>
2025-02-18 11:32:39 +0100

    first commit

---
+++ serie_4/exo4.5.js
@@ -0,0 +1,46 @@
+// console.log("Hello, word");
+// // document.write("Hello word")
+
+// let nombre = 42;
+
+// console.log('Le nombre est ' + nombre);
+
+// let user_input = prompt('Entrez un nom ')
+// console.log(user_input);
+
+// let resulat = somme(4,6);
+// console.log(resulat);
+
+// function somme (a,b) {
+//     return a + b;
+// }
+let age, genre;
+
+age = prompt("Quelle est votre âge ? ");
+genre = prompt("Quelle est votre genre ? ");
+
+if (    (genre == "draxx" && age > 1835 ) 
+    ||  (genre == "sklounst" && (age > 18 && age < 35))) 
+{
+    console.log("Ne peut pas voter");
+} else 
+{
+    console.log("Peut voter");
+} 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
