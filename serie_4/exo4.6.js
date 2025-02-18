a49d2bd9bbdf208f5ca39a87bc3165bc1f1f2c5d
Joffrey Sterckx <147731791+SterckxJoffrey@users.noreply.github.com>
2025-02-18 12:11:39 +0100

    change exo serie 4.6

--- serie_4/exo4.6.js
+++ serie_4/exo4.6.js
@@ -1,28 +1,22 @@
 console.log("enfkejfkenrfje");
 let C1, C2, C3, C4, C5;
 
-C1 = 25.5;
-C2 = 12.5
-C3 = 51;
-C4 = 11;
-
-
-
-
 if (C1 > 50)  {
+
     console.log("Elu");
 }
-else if((C1 < 12.5) || ((C2 > 50) || (C3 > 50) || (C4 > 50) )) {
+else if ((C1 < 12.5) || ((C2 > 50) || (C3 > 50) || (C4 > 50) )) {
     
     console.log("perdu");
  
-} else if ((C1 > 12.5) && ((C1 >= C2) && (C1 >= C3) && (C1 >= C4) ) ){
+} else if ((C1 >= C2) && (C1 >= C3) && (C1 >= C4) ) {
+
     console.log("Ballotage favorable");
-    
-        
+
 } else  {
+
     console.log("Ballotage dévaforable");
-    
+
 }
 
 
